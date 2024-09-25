d34fed6140cf8e4f4e59067f9550394ea3284396
Mythreya <m.kannan@automak.com>
2024-09-25 09:34:46 +0300

    Base Layout Commit 25/09/2024

---
+++ ajax_post.php
@@ -0,0 +1,27 @@
+  <?php
+  echo '<div class="element-item">
+                        <div class="tt-post">
+                            <div class="tt-post-img">
+                                <a href="blog-single-post.html"><img src="images/blog/blog_img_01.jpg" alt=""></a>
+                            </div>
+                            <div class="tt-post-content">
+                                <div class="tt-meta">
+                                    <div class="tt-time">
+                                        <span>16</span>
+                                        Apr
+                                    </div>
+                                    <div class="tt-autor">
+                                        Written by <span>Admin</span>
+                                    </div>
+                                </div>
+                                <h2 class="tt-title"><a href="blog-single-post.html">5 Important CRM Trends That Will Rev Up Business</a></h2>
+                                <div class="tt-description">
+                                    CRM trends come and go in the automotive industry, but these five are sure to improve your bottom line – and that’s a trend we can all appreciate.
+                                </div>
+                                <div class="tt-btn">
+                                    <a href="blog-single-post.html" class="tt-link">read more</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>';
+?>
